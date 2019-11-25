23f96076e3b8f0a0d7a38354f81d28a996c27b2a
Amit Patil <iamitpatil1993@gmail.com>
2019-11-25 09:15:41 +0530

    Enable MappingJackson2HttpMessageConvter to support JSON just by adding Jackson-Databinf lib [Nothing else required to configure]

--- src/main/java/com/example/mvc/revise/web/controller/EmployeeController.java
+++ src/main/java/com/example/mvc/revise/web/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.example.mvc.revise.web.controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import javax.servlet.http.HttpServletRequest;
 
