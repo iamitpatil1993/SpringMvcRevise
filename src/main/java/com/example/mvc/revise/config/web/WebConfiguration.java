e1974ebafbb714ab005d4d182c81f286641c21a1
Amit Patil <iamitpatil1993@gmail.com>
2019-12-22 11:57:59 +0530

    Customize/Override ObjectMappers of TypeConstrainedMappingJackson2HttpMessageConverter and MappingJackson2HttpMessageConverter

--- src/main/java/com/example/mvc/revise/config/web/WebConfiguration.java
+++ src/main/java/com/example/mvc/revise/config/web/WebConfiguration.java
@@ -3,12 +3,10 @@ package com.example.mvc.revise.config.web;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +14,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.hateoas.server.mvc.TypeConstrainedMappingJackson2HttpMessageConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -31,7 +30,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -39,6 +37,7 @@ import com.example.mvc.revise.config.support.RequestBodyToEntityProcessor;
 import com.example.mvc.revise.web.interceptor.AuthHandlerInterceptor;
 import com.example.mvc.revise.web.interceptor.LatencyCalculatorInterceptor;
 import com.example.mvc.revise.web.interceptor.RequestLoggingInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.example.mvc.revise.web", "com.example.mvc.revise.dto.assembler", "com.example.mvc.revise.dto.processor" })
@@ -106,18 +105,6 @@ public class WebConfiguration implements WebMvcConfigurer {
 		return commonsMultipartResolver;
 	}
 
-	@Bean
-	public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
-		return new MappingJackson2HttpMessageConverter();
-	}
-
-	@Bean
-	public RequestMappingHandlerAdapter mappingHandlerAdapter(final MappingJackson2HttpMessageConverter converter) {
-		RequestMappingHandlerAdapter requestMappingHandlerAdapter = new RequestMappingHandlerAdapter();
-		requestMappingHandlerAdapter.setMessageConverters(Arrays.asList(converter));
-		return requestMappingHandlerAdapter;
-	}
-
 	/**
 	 * Order of declaration of interceptors also defines there order of invocation.
 	 * Actually their default oder is zero unless specified. Hence all interceptors
@@ -179,13 +166,13 @@ public class WebConfiguration implements WebMvcConfigurer {
 		// application while serialization and deserialization. No need to use
 		// @JsonFormat in pojo to define date format, unless want to override.
 		// This way we can customize any existing HttpMessageConverter
-		converters.stream().filter(converter -> converter instanceof MappingJackson2HttpMessageConverter)
+		// I did not received TypeConstrainedMappingJackson2HttpMessageConverter in this
+		// converters list (don't know why), so could not customize it's ObjectMapper here by just
+		// changing filter
+		converters.stream().filter(converter -> (converter instanceof MappingJackson2HttpMessageConverter) || converter instanceof TypeConstrainedMappingJackson2HttpMessageConverter)
 		.forEach(converter -> {
 			MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = (MappingJackson2HttpMessageConverter) converter;
-			Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-			builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
-
-			jackson2HttpMessageConverter.setObjectMapper(builder.build());
+			jackson2HttpMessageConverter.setObjectMapper(objectMapper());
 		});
 		
 		// collect message converters for custom HandlerMethodArgumentResolver (RequestBodyToEntityProcessor) 
@@ -213,5 +200,37 @@ public class WebConfiguration implements WebMvcConfigurer {
 		registry.addMapping("/**").allowedOrigins("http://localhost:8088").allowedHeaders("customHeader");
 	}
 	
+	/**
+	 * Declare customized {@link ObjectMapper} in WebApplicationContext so that this
+	 * get picked up while creating beans of type Jackon message converter. Best
+	 * part is {@link TypeConstrainedMappingJackson2HttpMessageConverter} picks this
+	 * up, but {@link MappingJackson2HttpMessageConverter} does not.
+	 * {@link MappingJackson2HttpMessageConverter} is created using default
+	 * constructor by some internal code, so this object mapper not get passed to
+	 * it.
+	 * 
+	 * Hence this will work for
+	 * {@link TypeConstrainedMappingJackson2HttpMessageConverter} but not for
+	 * {@link MappingJackson2HttpMessageConverter} therefore we need to keep above
+	 * customization code (overriden extendMessageConverters()) as it for
+	 * {@link MappingJackson2HttpMessageConverter}
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ObjectMapper objectMapper() {
+		// we can avoid creating ObjectMapper like this, and use
+		// Jackson2ObjectMapperBuilder for simple building
+		/*
+		 * ObjectMapper mapper = new ObjectMapper();
+		 * mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		 * mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+		 */
+
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+		builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd")).failOnUnknownProperties(false);
+
+		return builder.build();
+	}
 
 }
